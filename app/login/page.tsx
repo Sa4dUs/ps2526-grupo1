95355a33d5fda6de55fd2566813187ed0144ec69
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-09-29 20:24:54 +0200

    Feedback
    I was asked to make visible the messages, now they are

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,12 +7,13 @@ import { useRouter } from 'next/navigation';
 export default function LogInPage(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');   
+    const [errorMessage, setErrorMessage] = useState("");
     const router = useRouter();
 
 const handleLoginClick= async()=>{ //we need async to be able to use wait later on.
     //try-catch is react version of angular's .valid
     if (!username || !password){
-        console.warn ("FILL THE FORM BASTARD");
+        setErrorMessage("The reason why you have fingers is the reason it failed :)");
         return;
     }
     try{
@@ -32,7 +33,7 @@ const handleLoginClick= async()=>{ //we need async to be able to use wait later 
         console.log("Login worked", data);
 
     }catch (err){
-        console.error("Something happened during the login."); 
+        setErrorMessage("Something happened during the login.");
     }
 }
 
@@ -40,7 +41,12 @@ const handleLoginClick= async()=>{ //we need async to be able to use wait later 
 return (
     <div>
         <h2>LogIn</h2>
-
+        
+        {errorMessage && (
+            <div style={{ color: "red", marginBottom: "1rem" }}>
+                {errorMessage}
+            </div>
+        )}
         <label>Username:</label>
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}
         />
