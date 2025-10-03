b876d4605e3216f3554f4ca421b37e3b9d577a4e
Marcelo Domínguez <dmmarcelo27@gmail.com>
2025-10-03 20:14:09 +0200

    Send full problem description

---
+++ types/problem.ts
@@ -0,0 +1,30 @@
+export type Solution = {
+	solution: number;
+	encoded: string;
+};
+
+export type RequestPayload = Solution | null;
+
+export enum ProblemError {
+	IncorrectSubmission = "IncorrectSubmission",
+	UnexpectedError = "UnexpectedError",
+	InvalidInput = "InvalidInput",
+}
+
+export type ResponseError = { error: ProblemError };
+
+export type ResponseSuccess = {
+	index: number;
+	question: string;
+	answers: number[];
+	encoded: string;
+};
+
+export type ResponsePayload = ResponseError | ResponseSuccess;
+
+export type Problem = {
+	index: number;
+	question: string;
+	answers: number[];
+	correctAnswer: number;
+};
