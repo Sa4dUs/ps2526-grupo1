c1f02e69047436147cd5af4db23567f99207a225
huugooocm <cmhugo307@gmail.com>
2025-10-10 18:57:06 +0200

    Training page with provisional css

--- app/training.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function TrainingPage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Training Mode</h1>
-      <p>Próximamente: problemas aleatorios.</p>
-    </main>
-  );
-}
