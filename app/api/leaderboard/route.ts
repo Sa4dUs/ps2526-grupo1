6f936f42ea058701419494279f7cca192bc9134d
Lamoara <jorginos40@gmail.com>
2025-11-10 17:36:32 +0100

    PG22-27-Changed single cuotes to double cuotes

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from 'next/server';
-import { get_leaderboard } from '@/lib/leaderboard';
+import { NextResponse } from "next/server";
+import { get_leaderboard } from "@/lib/leaderboard";
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
-    const from = Number(searchParams.get('from') ?? 0);
-    const to = Number(searchParams.get('to') ?? 100);
-    const user = searchParams.get('user') || undefined;
+    const from = Number(searchParams.get("from") ?? 0);
+    const to = Number(searchParams.get("to") ?? 100);
+    const user = searchParams.get("user") || undefined;
 
     try {
         const leaderboard = await get_leaderboard(from, to, user);
         return NextResponse.json(leaderboard);
     } catch (err) {
         console.error(err);
-        return NextResponse.json({ error: 'Error getting leaderboard' }, { status: 500 });
+        return NextResponse.json({ error: "Error getting leaderboard" }, { status: 500 });
     }
 }
