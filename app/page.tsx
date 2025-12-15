49f477718a5419d1c56a55ef3522d1e4d0df1e1f
PauCJ <p.calvo.2023@alumnos.urjc.es>
2025-12-15 18:51:44 +0100

    css
    css

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ import { Calculator, Timer, Flame, Plus, Code } from "lucide-react";
 export default function Home() {
 	return (
 		<div className="w-full flex flex-col items-center p-6">
-			<h2 className="text-3xl font-semibold mb-8">Choose de game mode</h2>
+			<h2 className="text-3xl font-semibold mb-8 border rounded-md p-2 font-titan">CHOOSE GAME MODE</h2>
 
 			<div className="w-full max-w-3xl flex flex-col gap-6">
 
 				{/* TRAIN */}
 				<Card className="w-full border rounded-3xl p-4 shadow-sm">
 					<div className="flex items-center gap-6">
-						<div className="p-4 bg-gray-100 rounded-2xl">
-							<Calculator className="w-16 h-16 text-gray-700" />
+						<div className="p-4 rounded-2xl" style={{ backgroundColor: '#77c9ffff' }}>
+							<Calculator className="w-16 h-16" style={{ stroke: '#102d42ff'}} />	
 						</div>
 						<div className="flex flex-col flex-grow">
 							<h3 className="text-2xl font-medium tracking-wide">Train</h3>
@@ -24,7 +24,7 @@ export default function Home() {
 								Train your math skills at your own pace
 							</p>
 							<Link href="/train">
-								<Button>Start</Button>
+								<Button style={{ backgroundColor: '#102d42ff' }}>Start</Button>
 							</Link>
 						</div>
 					</div>
@@ -33,8 +33,8 @@ export default function Home() {
 				{/* PUZZLE RUSH */}
 				<Card className="w-full border rounded-3xl p-4 shadow-sm">
 					<div className="flex items-center gap-6">
-						<div className="p-4 bg-gray-100 rounded-2xl">
-							<Flame className="w-16 h-16 text-gray-700" />
+						<div className="p-4 bg-gray-100 rounded-2xl" style={{ backgroundColor: '#ffcc9fff' }}>
+							<Flame className="w-16 h-16" style={{ stroke: '#e74242ff'}} />
 						</div>
 						<div className="flex flex-col flex-grow">
 							<h3 className="text-2xl font-medium tracking-wide">Puzzle Rush</h3>
@@ -42,7 +42,7 @@ export default function Home() {
 								Build the longest streak you can — one mistake ends it all.
 							</p>
 							<Link href="/puzzle">
-								<Button>Start</Button>
+								<Button style={{ backgroundColor: '#e74242ff' }}>Start</Button>
 							</Link>
 						</div>
 					</div>
@@ -51,8 +51,8 @@ export default function Home() {
 				{/* TIME TRIAL */}
 				<Card className="w-full border rounded-3xl p-4 shadow-sm">
 					<div className="flex items-center gap-6">
-						<div className="p-4 bg-gray-100 rounded-2xl">
-							<Timer className="w-16 h-16 text-gray-700" />
+						<div className="p-4 bg-gray-100 rounded-2xl" style={{ backgroundColor: '#afff9fff' }}>
+							<Timer className="w-16 h-16" style={{ stroke: '#6aa937ff'}} />
 						</div>
 						<div className="flex flex-col flex-grow">
 							<h3 className="text-2xl font-medium tracking-wide">Time Trial</h3>
@@ -60,7 +60,7 @@ export default function Home() {
 								How many problems can you solve in 30 seconds?
 							</p>
 							<Link href="/timetrial">
-								<Button>Start</Button>
+								<Button style={{ backgroundColor: '#6aa937ff' }}>Start</Button>
 							</Link>
 						</div>
 					</div>
@@ -69,8 +69,8 @@ export default function Home() {
 				{/* OPERATOR */}
 				<Card className="w-full border rounded-3xl p-4 shadow-sm">
 					<div className="flex items-center gap-6">
-						<div className="p-4 bg-gray-100 rounded-2xl">
-							<Plus className="w-16 h-16 text-gray-700" />
+						<div className="p-4 bg-gray-100 rounded-2xl" style={{ backgroundColor: '#dd9fffff' }}>
+							<Plus className="w-16 h-16" style={{ stroke: '#6837a9ff'}}/>
 						</div>
 						<div className="flex flex-col flex-grow">
 							<h3 className="text-2xl font-medium tracking-wide">Operator Challenge</h3>
@@ -78,7 +78,7 @@ export default function Home() {
 								Place each number and operator in the right spot to solve the equation.
 							</p>
 							<Link href="/operator">
-								<Button>Start</Button>
+								<Button style={{ backgroundColor: '#6837a9ff' }}>Start</Button>
 							</Link>
 						</div>
 					</div>
