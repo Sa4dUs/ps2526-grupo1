bb9aaf4218ff1b6ce15dc61070b54b2eca7640ad
Marcelo Domínguez <dmmarcelo27@gmail.com>
2025-12-18 12:51:42 +0100

    Use tailwind and adjust button colors

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="py-6 text-center text-sm text-muted-foreground border-t border-border" style={{ backgroundColor: '#ffffffff' }}>
+		<footer className="py-6 text-center text-sm text-muted-foreground border-t border-border bg-white">
 			© {new Date().getFullYear()} EduMath
 		</footer>
 	);
--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 	const { user } = useContext(AuthUserContext);
 
 	return (
-		<header className="flex items-center justify-between px-8 py-6 border-b border-border" style={{ backgroundColor: ' #ffffffff' }}>
+		<header className="flex items-center justify-between px-8 py-6 border-b border-border bg-white">
 			<Link href="/" className="flex items-center gap-3">
 				<Image src={"/logo.png"} alt="EduMath" width={50} height={50} />
 				<h1 className="text-2xl font-semibold tracking-tight">
